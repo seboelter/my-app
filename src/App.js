91fb9cfc42f7707eff95b796c26befd01cb647ea
Sarah Boelter <sboelter@mtaonline.net>
2020-05-22 19:46:32 -0800

    Changed app.js to a class structure, edited render function to return created element.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
+//import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     <h1>Hi, I'm a React App</h1>
-    </div>
-  );
+class App extends Component{
+  render(){
+    //Takes infinite amount of elements, but at least 3, html, js, children
+    return React.createElement('div', null, 'h1','Hi I\'m a React App!!');
+  }
 }
+
+// Below code was default, tutorial has class structure
+// function App() {
+//   return (
+//     // This is Jsx, syntactical sugar, because of build workflow
+//     <div className="App">
+//      <h1>Hi, I'm a React App</h1>
+//     </div>
+//   );
+// }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// Rendering root react componet
 ReactDOM.render(
   <React.StrictMode>
     <App />
